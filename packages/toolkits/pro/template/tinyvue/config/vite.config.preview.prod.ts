9e3910f17697fcb7c4196940143b8b931c2a5fe2
fengyon <feng.yg@foxmail.com>
2023-05-10 17:13:41 -0700

    fix(toolkit/pro): 修复提示文字错误问题

--- packages/toolkits/pro/template/tinyvue/config/vite.config.preview.prod.ts
+++ packages/toolkits/pro/template/tinyvue/config/vite.config.preview.prod.ts
@@ -2,18 +2,13 @@ import { mergeConfig } from 'vite';
 import baseConig from './vite.config.preview.base';
 import configCompressPlugin from './plugin/compress';
 import configVisualizerPlugin from './plugin/visualizer';
-import configArcoResolverPlugin from './plugin/arcoResolver';
 
 export default mergeConfig(
   {
     mode: 'pages',
     mock: true,
     base: `${process.env.static_url_prefix}/tiny-pro-vue/${process.env.staticReleaseVersion}/vue-pro/pages`,
-    plugins: [
-      configCompressPlugin('gzip'),
-      configVisualizerPlugin(),
-      configArcoResolverPlugin(),
-    ],
+    plugins: [configCompressPlugin('gzip'), configVisualizerPlugin()],
     build: {
       rollupOptions: {
         output: {
