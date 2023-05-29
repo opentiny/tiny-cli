7d96ee821d58ff679a9745286e3a42918b3aaea9
h-ivy <opentiny@sina.com>
2023-05-29 20:41:48 +0800

    add csrf config

--- packages/toolkits/pro/template/server/eggJs/config/config.local.ts
+++ packages/toolkits/pro/template/server/eggJs/config/config.local.ts
@@ -2,5 +2,9 @@ import { EggAppConfig, PowerPartial } from 'egg';
 
 export default () => {
   const config: PowerPartial<EggAppConfig> = {};
+
+  config.security = {
+    csrf: false,
+  };
   return config;
 };
