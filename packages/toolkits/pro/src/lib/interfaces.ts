ee1c324204c698fd1c4968b98ba1409dcada235b
wjiangwang <39005591+wjiangwang@users.noreply.github.com>
2023-07-07 14:37:38 +0800

    fix: pro init 流程完善 (#73)
    * fix: pro init 流程完善
    
    * docs: update pro doc

--- packages/toolkits/pro/src/lib/interfaces.ts
+++ packages/toolkits/pro/src/lib/interfaces.ts
@@ -24,6 +24,7 @@ export interface ProjectInfo {
   framework: string;
   name: string;
   serverFramework: ServerFrameworks;
+  serverConfirm?: boolean;
   dialect?: string;
   host?: string;
   port?: number;
