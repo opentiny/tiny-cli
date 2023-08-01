d0042b74ebc72fcdccfdfdf94213e59d9ff852c3
OpenTiny <opentiny@sina.com>
2023-08-01 16:26:43 +0800

    feat: tinyng主题配置-主题优化 WI2023
    Match-id-983293f94b7c1a3aaa3557d81895f0d74803ae38

--- packages/toolkits/pro/template/tinyng/src/app/@core/services/custom-theme.service.ts
+++ packages/toolkits/pro/template/tinyng/src/app/@core/services/custom-theme.service.ts
@@ -3,26 +3,18 @@ import { ThemeAddrPrefix, ThemeType } from 'src/app/@shared/models/theme';
 import { CustomStyle, textWhiteStyle } from '@config/tiny-pro';
 
 export class CustomThemeService {
-  changeCustomTheme(color: string, isDark: boolean) {
+  changeCustomTheme(color: string, isDark: boolean, themeId:any) {
     if (isDark) {
       setTimeout(() => {
         TiTheme.loadCss(
-          `${ThemeAddrPrefix}/tiny-dark-theme.css`,
+          `${ThemeAddrPrefix}/${themeId}.css`,
           'tiny3theme'
         );
       });
     } else {
-      TiTheme.loadCss(
-        `${ThemeAddrPrefix}/tiny-hws-theme.css`,
-        'tiny3theme'
-      );
+      // 加载默认主题不传入路径避免报错
+      TiTheme.loadCss('', 'tiny3theme');
     }
-
-    for (const style of CustomStyle) {
-      document.documentElement.style.setProperty(style, color);
-      document.body.style.setProperty('--ti-leftmenu-item-selected-border-left-color', 'red');
-    }
-
     // 自定义主题下显示字体颜色设置为#fff
     for (const style of textWhiteStyle) {
       document.documentElement.style.setProperty(style, '#fff');
