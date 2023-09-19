5ed766bda35a883ef7865ae2eb84b87e6793d110
fengyon <84690330+fengyon@users.noreply.github.com>
2023-09-19 03:50:31 -0700

    fix: fix 6 issues  (#103)
    * fix(toolkit-pro): fix incorrect command options of ng template
    
    remove command options of mock and local server for ng template
    
    fix #94; fix #98
    
    * fix(toolkit-pro/eggjs): fix the error of eggjs template when run `npm start`
    
    fix #99
    
    * fix(toolkit-pro): change the tips for starting the local server of spring cloud
    
    because starting the spring cloud that is not support by run `npm run dev`
    
    fix #102
    
    * docs(toolkit-pro/springcloud): remove the link of ces 1.0
    
    because ces 1.0 is no longer supported by huawei cloud
    
    fix #101
    
    * fix(cli-devkit): optimize the cwd of installing and the message
    
    optimize the cwd of install to the options. And optimize the prompting after input error package in
    update.
    
    fix #97
    
    * docs(toolkit-pro): add change log

--- packages/toolkits/pro/src/lib/init.ts
+++ packages/toolkits/pro/src/lib/init.ts
@@ -68,7 +68,9 @@ const getProjectInfo = (): Promise<ProjectInfo> => {
         { name: '暂不配置服务端', value: false },
       ],
       prefix: '*',
-      when: (answers) => answers.serverFramework !== ServerFrameworks.Skip,
+      when: (answers) =>
+        answers.framework === VUE_TEMPLATE_PATH &&
+        answers.serverFramework !== ServerFrameworks.Skip,
     },
     {
       type: 'list',
@@ -255,6 +257,11 @@ const createProjectSync = (answers: ProjectInfo) => {
     log.error('配置项目信息创失败');
   }
 
+  // ng模板不开启mock以及服务
+  if (templatePath === NG_TEMPLATE_PATH) {
+    return;
+  }
+
   // 如果不对接服务端，全部接口采用mock
   if (!serverConfirm) {
     try {
@@ -316,7 +323,9 @@ export const installDependencies = (answers: ProjectInfo) => {
     console.log(
       chalk.green(
         `${chalk.yellow(
-          `$ cd ${name}/${serverFramework} && npm run dev`
+          serverFramework === ServerFrameworks.SpringCloud
+            ? `请查看 ${name}/${serverFramework}/README_CN.md `
+            : `$ cd ${name}/${serverFramework} && npm run dev`
         )}    # 开启server开发环境`
       )
     );
